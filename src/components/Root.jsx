e2ef25af643f9c29d846d93a3d12ae998b44f6fe
Shahinul99 <rumaisaabrar99@gmail.com>
2025-09-05 19:23:10 -0400

    first commit

---
+++ src/components/Root.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Root = () => {
+    return (
+        <div>
+            <h3 className="text-5xl"> HELLO from Root </h3>
+        </div>
+    )
+}
+
+export default Root;
